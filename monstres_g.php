0d35e9db3819cd92aaeaf0f19acc5f4f7f9c1ada
SenuinLentar <myal64@free.fr>
2018-05-08 13:18:05 +0200

    add 2 kind of bdd connection --> online and offline

--- monstres_g.php
+++ monstres_g.php
@@ -1,14 +1,9 @@
 <?php
 session_start();
 
-try
-{
-    $bdd = new PDO('mysql:host=178.62.4.64;dbname=Re:Monster_M;charset=utf8', 'Administrateur', 'maxime1', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-}
-catch (Exception $e)
-{
-    die ('Erreur : ' . $e->getMessage());
-}
+require ("connexion_bdd.php");
+
+$bdd = getBDD();
 ?>
 <!doctype html>
 <html>
